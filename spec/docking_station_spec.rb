713d230afb51970305ad947a3968abd44c74e6c6
christymasterson <christymasterson@gmail.com>
2019-12-10 16:03:56 +0000

    Adding changes to spec files end of step 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require 'docking_station'
 
+#regular way of writing
 describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 
 end
