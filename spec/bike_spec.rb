58c18d303ea2469bdf5f1048930f99fd84f1b51d
christymasterson <christymasterson@gmail.com>
2019-12-11 16:37:26 +0000

    Adding spec file at end of Step 14

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,9 @@ require 'bike'
 
 describe Bike do
 it { is_expected.to respond_to :working? }
+
+
+
+# it { is_expected.to raise_error "No bikes available" }
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,24 +3,33 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  # it 'releases working bikes' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
+
+  describe '#dock' do
+    it 'raises an error when docking station is full' do
+      20.times {subject.dock(Bike.new)}
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
   end
+
+  describe '#release_bike' do
+    it 'raises an error where there are no bikes available' do
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+end
+
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
 
-  # it 'docks something' do
+  # it 'returns docked bikes' do
   #   bike = Bike.new
-  #   expect(subject.dock(bike)).to eq bike
+  #   subject.dock(bike)
+  #   expect(subject.bike).to eq bike
   # end
-  
-it 'returns docked bikes' do
-  bike = Bike.new
-  subject.dock(bike)
-  expect(subject.bike).to eq bike
-end
 
 end
