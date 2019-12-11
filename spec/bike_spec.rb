cf77a6a5c7d0f6a4755d0bf72f6ba42c48d36b7d
christymasterson <christymasterson@gmail.com>
2019-12-11 17:11:43 +0000

    Adding spec file at end of step 16

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,10 +1,10 @@
 require 'bike'
 
 describe Bike do
-it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to :working? }
 
 
 
-# it { is_expected.to raise_error "No bikes available" }
+  # it { is_expected.to raise_error "No bikes available" }
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,16 +10,18 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when docking station is full' do
-      20.times {subject.dock(Bike.new)}
+      DockingStation::DEFAULT_CAPACITY.times do
+        subject.dock(Bike.new)
+      end
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 
   describe '#release_bike' do
     it 'raises an error where there are no bikes available' do
-    expect { subject.release_bike }.to raise_error 'No bikes available'
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
-end
 
 
   it { is_expected.to respond_to(:dock).with(1).argument }
